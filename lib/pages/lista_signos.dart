50b789bbb4ff8588ae7c75e043dd116ce144bd87
rochac2lee <cleberli@dgsolutions.com.br>
2021-03-03 09:29:35 -0300

    Implementação do Widget CardSigno

--- lib/pages/lista_signos.dart
+++ lib/pages/lista_signos.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:signos/utils/card_signo.dart';
+import '../data/dados_signos.dart' as colecao_signos;
 
 class ListaSignos extends StatelessWidget {
+  var signos = colecao_signos.signos;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +15,10 @@ class ListaSignos extends StatelessWidget {
       body: GridView.count(
         padding: EdgeInsets.only(top: 20),
         crossAxisCount: 2,
-        children: List.generate(1, (index) => CardSigno()),
+        children: List.generate(
+          signos.length,
+          (index) => CardSigno(signos[index]),
+        ),
       ),
     );
   }
--- lib/utils/card_signo.dart
+++ lib/utils/card_signo.dart
@@ -1,40 +1,39 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:signos/pages/detalhes_signo.dart';
 
 class CardSigno extends StatelessWidget {
+  String _nome;
+  String _imagem;
+  Map _dataSigno;
+
+  CardSigno(Map signo) {
+    this._nome = signo["Nome"];
+    this._imagem = signo["Imagem"];
+    this._dataSigno = signo;
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return RaisedButton(
       padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
       color: Colors.white,
       child: Column(
         children: [
-
-          Image.asset("img/aquario.jpg", width: 90),
-
+          Image.asset("$_imagem", width: 90),
           SizedBox(height: 10),
-          
-          Text(
-            "Aquário", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), textAlign: TextAlign.center
-          ),
-
+          Text("$_nome",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center),
           SizedBox(height: 10),
-
         ],
       ),
       onPressed: () {
         Navigator.push(
-          context,
-          MaterialPageRoute( 
-            builder: (context) => DetalhesSigno()
-          )
-        );
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetalhesSigno(this._dataSigno),
+            ));
       },
     );
-
   }
 }
