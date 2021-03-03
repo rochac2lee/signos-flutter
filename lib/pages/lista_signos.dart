32d8b67113ef2c8a33cbdfc6007748f6c3e8698a
rochac2lee <cleberli@dgsolutions.com.br>
2021-03-03 08:28:16 -0300

    Inserindo um GridView para listar todos os signos

--- lib/pages/lista_signos.dart
+++ lib/pages/lista_signos.dart
@@ -1,22 +1,19 @@
-
 import 'package:flutter/material.dart';
+import 'package:signos/utils/card_signo.dart';
 
 class ListaSignos extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Características dos Signos"),
         centerTitle: true,
       ),
-      body: Container()
-
+      body: GridView.count(
+        padding: EdgeInsets.only(top: 20),
+        crossAxisCount: 2,
+        children: List.generate(1, (index) => CardSigno()),
+      ),
     );
-
   }
 }
-
-
-
