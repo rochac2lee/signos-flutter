e53f832b752a1cb4c297e59e410877fb72733754
rochac2lee <cleberli@dgsolutions.com.br>
2021-03-03 09:42:06 -0300

    Implementação da view Detalhes do Signo

--- lib/pages/detalhes_signo.dart
+++ lib/pages/detalhes_signo.dart
@@ -1,44 +1,49 @@
-
 import 'package:flutter/material.dart';
 
 class DetalhesSigno extends StatelessWidget {
+  String _nome;
+  String _dataInicio;
+  String _dataFim;
+  String _imagem;
+  String _descricao;
+
+  DetalhesSigno(Map signo) {
+    this._nome = signo['Nome'];
+    this._imagem = signo['Imagem'];
+    this._dataInicio = signo['DataInicio'];
+    this._dataFim = signo['DataFim'];
+    this._descricao = signo['Descricao'];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       title: Text("Signo: Aquário"),
-       centerTitle: true,
-     ),
-     body: Center(
+      appBar: AppBar(
+        title: Text("Signo: Aquário"),
+        centerTitle: true,
+      ),
+      body: Center(
         child: Padding(
           padding: EdgeInsets.all(25),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
-              Image.asset("img/aquario.jpg", width: 200),
-
+              Image.asset("$_imagem", width: 200),
               SizedBox(height: 30),
-              
-              Text(
-                "Aquário", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold), textAlign: TextAlign.center
-              ),
-
+              Text("$_nome",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center),
               SizedBox(height: 15),
-
-              Text(
-                "21/01 - 19/02", style: TextStyle(fontSize: 18)
-              ),
-
+              Text("$_dataInicio - $_dataFim", style: TextStyle(fontSize: 18)),
               SizedBox(height: 30),
-
-              Text('Características: ', style: TextStyle( color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold)),
-              
+              Text('Características: ',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold)),
               SizedBox(height: 10),
-
-              Text("Inovação, originalidade e independência", style: TextStyle( color: Colors.black, fontSize: 16))
-
+              Text("$_descricao",
+                  style: TextStyle(color: Colors.black, fontSize: 16))
             ],
           ),
         ),
